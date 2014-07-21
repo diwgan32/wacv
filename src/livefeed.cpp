532e8aa4aaac99d3412d5d82354d5e139df1c82c
Chandrashekar Ganesan <cng2@nist.gov>
2014-07-21 13:12:50 -0400

    Added loop to reduce data read times

--- src/livefeed.cpp
+++ src/livefeed.cpp
@@ -44,197 +44,199 @@ void convertAndResize(const T& src, T& gray, T& resized, double scale)
 
 int main(int argc, const char *argv[])
 {
+
+	ifstream fin;
+	Mat ImgData;
+
+	cout << "Reading Data....";
+	ImgData = readBin("DataFiles\\HONDA_DATA.bin", 400, 100);
+	cout << "Done" << endl;
+
+	fin.open("DataFiles\\subjectFrameNums.txt", std::ifstream::in);
 	cout << "Train or recog?: ";
 	int x;
 	cin >> x;
+	cout << endl;
+	while(x!=3){
+		int pos = 0;
+		ofstream fout;
+		int numSegments = 3;
 
-	int pos = 0;
-	ofstream fout;
-	int numSegments = 3;
-	ifstream fin;
-	Mat ImgData;
-	switch (x){
 
-	case 0:
 
-		cout << "Reading Data....";
-		ImgData = readBin("DataFiles\\HONDA_DATA.bin", 400, 4966);
-		cout << "Done" << endl;
+		switch (x){
 
-		fin.open("DataFiles\\subjectFrameNums.txt", std::ifstream::in);
+		case 0:
 
-		for(int z = 0; z<15; z++){
-			// 249 306 163 240 263
-			MATFile *pmat;
-			mxArray *pa1;
-			int size = 0;
-			fin >> size;
-			cout << size <<" ";
-			size = size-1;
 
-			Mat portion = ImgData.colRange(Range(pos, pos+100));
-			pos+=size;
-			size = 100;
-			Groupings g = seg1(portion, numSegments);
+			for(int z = 0; z<15; z++){
+				// 249 306 163 240 263
+				MATFile *pmat;
+				mxArray *pa1;
+				int size = 0;
+				fin >> size;
+				cout << size <<" ";
+				size = size-1;
 
-			int maxnum = 0;
-			for(int j = 0; j<numSegments; j++){
+				Mat portion = ImgData.colRange(Range(pos, pos+100));
+				pos+=size;
+				size = 100;
+				Groupings g = seg1(portion, numSegments);
 
-				if(g.segments[j].size()>maxnum){
-					maxnum = g.segments[j].size();
-				}
-			}
-			pa1 = mxCreateDoubleMatrix(numSegments, maxnum, mxREAL);
-			string filename = "Segments\\"+itos(z)+".mat";
-			pmat = matOpen(filename.c_str(), "w");
+				int maxnum = 0;
+				for(int j = 0; j<numSegments; j++){
 
-			double * data2;
-			int count = 0;
-			data2 = new double[numSegments*maxnum];
-			for(int j = 0; j<maxnum; j++){
-				for(int k = 0; k<numSegments;  k++){
-					if(j>=g.segments[k].size()){
-						data2[count] = -1;
+					if(g.segments[j].size()>maxnum){
+						maxnum = g.segments[j].size();
 					}
-					else{
-						data2[count] = g.segments[k].at(j);
+				}
+				pa1 = mxCreateDoubleMatrix(numSegments, maxnum, mxREAL);
+				string filename = "Segments\\"+itos(z)+".mat";
+				pmat = matOpen(filename.c_str(), "w");
+
+				double * data2;
+				int count = 0;
+				data2 = new double[numSegments*maxnum];
+				for(int j = 0; j<maxnum; j++){
+					for(int k = 0; k<numSegments;  k++){
+						if(j>=g.segments[k].size()){
+							data2[count] = -1;
+						}
+						else{
+							data2[count] = g.segments[k].at(j);
+						}
+						count++;
 					}
-					count++;
-				}
-			}
-
-			memcpy((void *)(mxGetPr(pa1)), data2, sizeof(data2)*maxnum*numSegments);
-			int status = matPutVariable(pmat, "Segments", pa1);
-			if (status != 0) {
-
-				printf("%s :  Error using matPutVariable on line %d\n", __FILE__, __LINE__);
-				return(EXIT_FAILURE);
-			}  
-
-			mxDestroyArray(pa1);
-			matClose(pmat);
-
-
-
-			pa1 = mxCreateDoubleMatrix(400, size, mxREAL);
-			filename = "Subjects\\"+itos(z)+".mat";
-			pmat = matOpen(filename.c_str(), "w");
-
-			double * data1;
-			data1 = new double[size*400];
-			count = 0;
-			for(int i = 0; i<size; i++){
-				for(int j = 0; j<400; j++){
-					data1[count] = portion.at<double>(j, i);
-					count++;
 				}
 
+				memcpy((void *)(mxGetPr(pa1)), data2, sizeof(data2)*maxnum*numSegments);
+				int status = matPutVariable(pmat, "Segments", pa1);
+				if (status != 0) {
+
+					printf("%s :  Error using matPutVariable on line %d\n", __FILE__, __LINE__);
+					return(EXIT_FAILURE);
+				}  
+
+				mxDestroyArray(pa1);
+				matClose(pmat);
+
+
+
+				pa1 = mxCreateDoubleMatrix(400, size, mxREAL);
+				filename = "Subjects\\"+itos(z)+".mat";
+				pmat = matOpen(filename.c_str(), "w");
+
+				double * data1;
+				data1 = new double[size*400];
+				count = 0;
+				for(int i = 0; i<size; i++){
+					for(int j = 0; j<400; j++){
+						data1[count] = portion.at<double>(j, i);
+						count++;
+					}
+
+				}
+
+
+				memcpy((void *)(mxGetPr(pa1)), data1, sizeof(data1)*400*size);
+				status = matPutVariable(pmat, "SubjectData", pa1);
+				if (status != 0) {
+
+					printf("%s :  Error using matPutVariable on line %d\n", __FILE__, __LINE__);
+					return(EXIT_FAILURE);
+				}  
+
+				mxDestroyArray(pa1);
+				matClose(pmat);
+
+				cout  << z << endl;
+				delete data1;
+				delete data2;
 			}
+			fin.close();
+			break;
+
+		case 1:
+
+			pos = 0;
 
 
-			memcpy((void *)(mxGetPr(pa1)), data1, sizeof(data1)*400*size);
-			status = matPutVariable(pmat, "SubjectData", pa1);
-			if (status != 0) {
+			for(int z = 0; z<15; z++){
+				// 249 183 292 306 249
+				MATFile *pmat;
+				mxArray *pa1;
+				int size = 0;
+				fin >> size;
+				size = size-1;
 
-				printf("%s :  Error using matPutVariable on line %d\n", __FILE__, __LINE__);
-				return(EXIT_FAILURE);
-			}  
+				Mat portion = ImgData.colRange(Range(pos+100, pos+150));
+				pos+=size;
+				size = 50;
+				Groupings g = seg1(portion, numSegments);
+				vector<Mat> split = splitBySegment(portion, g);
+				//cout << portion << endl;
+				ifstream ifs("Dictionaries\\dict.bin", ios::binary);
+				vector<Mat> dict;
+				for(int i = 0; i<NUM_DICT; i++){
+					dict.push_back(Mat (ROWS, COLS, CV_64F));
+				}
+				double val;
+				for(int i = 0; i<COLS*NUM_DICT; i++){
+					for(int j = 0; j<ROWS; j++){
+						ifs.read(reinterpret_cast<char*> (&dict.at(i/COLS).at<double>(j, i%COLS)) , sizeof val);
+					}
 
-			mxDestroyArray(pa1);
-			matClose(pmat);
+				}
+				ifs.close();
 
-			cout  << z << endl;
-			delete data1;
-			delete data2;
+				ifstream ifs1("Dictionaries\\pinvDict.bin", ios::binary);
+				vector<Mat> pinvD;
+				for(int i = 0; i<NUM_DICT; i++){
+					pinvD.push_back(Mat(COLS, ROWS, CV_64F));
+				}
+				for(int i = 0; i<ROWS*NUM_DICT; i++){
+					for(int j = 0; j<COLS; j++){
+						ifs1.read(reinterpret_cast<char*> (&pinvD.at(i/ROWS).at<double>(j, i%ROWS)) , sizeof val);
+					}
+				}
+				ifs1.close();
+				vector<int> scores;
+				scores.reserve(NUM_SUBJECTS);
+				int max = 0;
+				int id = 0;
+				for(int i = 0; i<NUM_SUBJECTS; i++){
+					scores.push_back(0);
+				}
+				for(int i = 0; i<numSegments; i++){
+					Mat labels = image_test(split.at(i), dict, pinvD);
+
+
+					for(int j = 0; j<labels.rows; j++){
+						scores.at(labels.at<int>(j, 0)) += 1;
+					}
+
+					max = 0;
+					id = 0;
+					for(int j = 0; j<NUM_SUBJECTS; j++){
+						if(scores.at(j) > max){
+							max = scores.at(j);
+							id = j;
+						}
+					}
+				}
+				sort(scores.begin(), scores.end(), wayToSort);
+				cout << "Subject: #"<<id << endl;
+
+			}
+			fin.close();
+			break;
 		}
-		fin.close();
-		break;
-
-	case 1:
-		cout << "Reading Data....";
-		ImgData = readBin("DataFiles\\HONDA_DATA.bin", 400, 4966);
-		cout << "Done" << endl;
-
-		fin.open("DataFiles\\subjectFrameNums.txt", std::ifstream::in);
-
-		pos = 0;
-
-
-		for(int z = 0; z<15; z++){
-			// 249 183 292 306 249
-			MATFile *pmat;
-			mxArray *pa1;
-			int size = 0;
-			fin >> size;
-			size = size-1;
-
-			Mat portion = ImgData.colRange(Range(pos+100, pos+150));
-			pos+=size;
-			size = 50;
-			Groupings g = seg1(portion, numSegments);
-			vector<Mat> split = splitBySegment(portion, g);
-			//cout << portion << endl;
-			ifstream ifs("Dictionaries\\dict.bin", ios::binary);
-			vector<Mat> dict;
-			for(int i = 0; i<NUM_DICT; i++){
-				dict.push_back(Mat (ROWS, COLS, CV_64F));
-			}
-			double val;
-			for(int i = 0; i<COLS*NUM_DICT; i++){
-				for(int j = 0; j<ROWS; j++){
-					ifs.read(reinterpret_cast<char*> (&dict.at(i/COLS).at<double>(j, i%COLS)) , sizeof val);
-				}
-
-			}
-			ifs.close();
-
-			ifstream ifs1("Dictionaries\\pinvDict.bin", ios::binary);
-			vector<Mat> pinvD;
-			for(int i = 0; i<NUM_DICT; i++){
-				pinvD.push_back(Mat(COLS, ROWS, CV_64F));
-			}
-			for(int i = 0; i<ROWS*NUM_DICT; i++){
-				for(int j = 0; j<COLS; j++){
-					ifs1.read(reinterpret_cast<char*> (&pinvD.at(i/ROWS).at<double>(j, i%ROWS)) , sizeof val);
-				}
-			}
-			ifs1.close();
-			vector<int> scores;
-			scores.reserve(NUM_SUBJECTS);
-			int max = 0;
-			int id = 0;
-			for(int i = 0; i<NUM_SUBJECTS; i++){
-				scores.push_back(0);
-			}
-			for(int i = 0; i<numSegments; i++){
-				Mat labels = image_test(split.at(i), dict, pinvD);
-
-
-				for(int j = 0; j<labels.rows; j++){
-					scores.at(labels.at<int>(j, 0)) += 1;
-				}
-
-				max = 0;
-				id = 0;
-				for(int j = 0; j<NUM_SUBJECTS; j++){
-					if(scores.at(j) > max){
-						max = scores.at(j);
-						id = j;
-					}
-				}
-			}
-			sort(scores.begin(), scores.end(), wayToSort);
-			cout << "Subject: #"<<id << endl;
-
-		}
-		fin.close();
-		int a;
-	cin >> a;
-		break;
+		cout << "Train or recog?: ";
+		int x;
+		cin >> x;
+		cout << endl;
 	}
 
-
-	
 
 	return 0;
 }
